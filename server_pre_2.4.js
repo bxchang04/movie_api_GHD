261c903a52ceb94b15f5e0aea27bae89a3c6df57
bxchang04 <bxchang04@gmail.com>
2019-12-02 20:44:00 -0500

    update 2.2 sever_pre_2.4.js

--- server_pre_2.4.js
+++ server_pre_2.4.js
@@ -1,5 +1,3 @@
-console.log('My first Node test server is running on Port 8080.');
-
 const http = require('http'),
   fs = require('fs'),
   url = require('url');
@@ -15,12 +13,15 @@ http.createServer((request, response) => {
     filePath = 'index.html';
   }
 
-  fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', function(err) {
+  fs.readFile(filePath, function(err, data) {
     if (err) {
-      console.log(err);
+      throw err;
     }
-      else {
-    console.log('Added to log.');
-    }
+
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(data);
+    response.end();
+
   });
+
 }).listen(8080);
