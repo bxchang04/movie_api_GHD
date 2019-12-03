034a06750fe3acde3b7a415e62fd7a86ebc6df63
bxchang04 <bxchang04@gmail.com>
2019-12-03 12:24:05 -0500

    2.2 update scripts pre 2.4

--- server_pre_2.4.js
+++ server_pre_2.4.js
@@ -21,7 +21,7 @@ http.createServer((request, response) => {
     }
 
     response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write(data);
+    //response.write(data);
     response.end();
 
   });
