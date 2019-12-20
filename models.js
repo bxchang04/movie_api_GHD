d6920479e621c405809f883a136261c9c89fe604
Benjamin Chang <bxchang04@gmail.com>
2019-12-19 22:09:07 -0500

    attempt to push through correct index file

--- models.js
+++ models.js
@@ -16,6 +16,8 @@ var movieSchema = mongoose.Schema({
   Featured : Boolean
 });
 
+const bcrypt = require('bcrypt');
+
 var userSchema = mongoose.Schema({
  Username : {type: String, required: true},
  Password : {type: String, required: true},
@@ -23,6 +25,13 @@ var userSchema = mongoose.Schema({
  Birthday : Date,
  FavoriteMovies : [{ type : mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
+
+userSchema.statics.hashPassword = function(password) {
+  return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.Password); };
 
 var Movie = mongoose.model('Movie', movieSchema);
 var User = mongoose.model('User', userSchema);
