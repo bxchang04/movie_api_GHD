c075fc503cb82f8475607f372d3f4b7b250519c1
bxchang04 <bxchang04@gmail.com>
2019-12-24 17:32:03 -0500

    update 3.3

---
+++ client/src/components/movie-view/movie-view3.4+.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+//is this the correct way to create a button?
+import Button from 'react-bootstrap/Button';
+
+export class MovieView extends React.Component {
+
+    constructor() {
+        super();
+
+        this.state = {};
+    }
+
+    render() {
+        const { movie } = this.props;
+
+        if (!movie) return null;
+
+        return (
+            <div className="movie-view">
+                <img className="movie-poster" src={movie.ImagePath} />
+                <div className="movie-title">
+                    <span className="label">Title: </span>
+                    <span className="value">{movie.Title}</span>
+                </div>
+                <div className="movie-description">
+                    <span className="label">Description: </span>
+                    <span className="value">{movie.Description}</span>
+                </div>
+
+                <div className="movie-genre">
+                    <span className="label">Genre: </span>
+                    <span className="value">{movie.Genre.Name}</span>
+                </div>
+                <div className="movie-director">
+                    <span className="label">Director: </span>
+                    <span className="value">{movie.Director.Name}</span>
+                </div>
+            </div>
+
+            //is the ordering for this button right? How do I implement the rest of this button feature?
+            <div className="my-flix">
+                <Button />
+            </div>
+        );
+    }
+}
