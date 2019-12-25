1641eba047f970c560bdd6c89ba2e830a51ced11
bxchang04 <bxchang04@gmail.com>
2019-12-24 22:36:09 -0500

    update 3.2 and 3.3

--- client/src/components/movie-view/movie-view3.3.jsx
+++ client/src/components/movie-view/movie-view3.3.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import axios from 'axios';
-
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+// import Button from 'react-bootstrap/Button';
 
 export class MainView extends React.Component {
 
@@ -40,7 +40,23 @@ export class MainView extends React.Component {
            <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)}/>
          ))
       }
+      //back button from https://jsfiddle.net/2fmbfn41/2/
+      {this.state.showBack?
+        <a onClick={()=>hashHistory.goBack()}>&#8592; </a>
+        :<a className="back-btn">&#9776;</a>
+       }
      </div>
+
     );
   }
 }
+
+//back button from https://jsfiddle.net/2fmbfn41/2/
+ReactDOM.render(
+  <Router history={hashHistory}>
+       <Route path="/" component={App} >
+            <Route path="page1" component={Page1}/>
+            <Route  path="page2" component={Page2}/>
+            <Route  path="page3" component={Page3}/>
+       </Route>
+</Router>, document.querySelector('#app'));
