d7c3dcf32e08ad66d2179bc0b01eec1be5ebe581
bxchang04 <bxchang04@gmail.com>
2020-01-02 22:09:52 -0500

    update 3.4

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -33845,7 +33845,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65498" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51504" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
---
+++ client/src/components/movie-card/movie-card3.4.jsx
@@ -0,0 +1,23 @@
+// client/src/components/movie-card/movie-card.jsx
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+export class MovieCard extends React.Component {
+  render() {
+    const { movie, onClick } = this.props;
+
+    return (
+      <Card style={{ width: '16rem' }}>
+        <Card.Img variant="top" src={movie.ImagePath} />
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Text>{movie.Description}</Card.Text>
+          <Button onClick={() => onClick(movie)} variant="link">Open</Button>
+        </Card.Body>
+      </Card>
+    );
+  }
+}
