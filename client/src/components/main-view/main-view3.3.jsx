de288a9b98f8e288cb3c5690d72a02e781f8fce6
bxchang04 <bxchang04@gmail.com>
2020-01-02 20:29:31 -0500

    update 3.3

--- client/src/components/main-view/main-view3.3.jsx
+++ client/src/components/main-view/main-view3.3.jsx
@@ -25,19 +25,17 @@ export class MainView extends React.Component {
 
 
   render() {
-    // If the state isn't initialized, this will throw on runtime
-    // before the data is initially loaded
-    const { movies } = this.state;
+     const { movies } = this.state;
 
-    // Before the movies have been loaded
-    if (!movies) return <div className="main-view"/>;
+     // Before the movies have been loaded
+     if (!movies) return <div className="main-view"/>;
 
-    return (
-     <div className="main-view">
-     { movies.map(movie => (
-       <div className="movie-card" key={movie._id}>{movie.Title}</div>
-     ))}
-     </div>
-    );
-  }
-}
+     return (
+      <div className="main-view">
+      { movies.map(movie => (
+        <MovieCard key={movie._id} movie={movie}/>
+      ))}
+      </div>
+     );
+   }
+ }
