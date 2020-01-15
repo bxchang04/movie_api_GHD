f0de168b5e7db7f7a2d0219385bd6b4a78b5696c
bxchang04 <bxchang04@gmail.com>
2020-01-15 15:30:34 -0500

    update 3.5

--- client/src/components/profile-view/profile-view.jsx
+++ client/src/components/profile-view/profile-view.jsx
@@ -1,19 +1,17 @@
-//deleting favs works
-//displaying fav titles, deleting users does not
+import React from 'react';
+import axios from 'axios';
 
-import React from "react";
-import axios from "axios";
-
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import Container from "react-bootstrap/Container";
-import ListGroup from "react-bootstrap/ListGroup";
-import "./profile-view.scss";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import ListGroup from 'react-bootstrap/ListGroup';
+import './profile-view.scss'
 // import profileLogo from '/images/user_icon.svg';
 
 import { Link } from "react-router-dom";
 
 export class ProfileView extends React.Component {
+
   constructor() {
     super();
     this.state = {
@@ -28,18 +26,17 @@ export class ProfileView extends React.Component {
 
   componentDidMount() {
     //authentication
-    let accessToken = localStorage.getItem("token");
+    let accessToken = localStorage.getItem('token');
     if (accessToken !== null) {
       this.getUser(accessToken);
     }
   }
 
   getUser(token) {
-    let username = localStorage.getItem("user");
-    axios
-      .get(`https://myFlixDB2.herokuapp.com/users/${username}`, {
-        headers: { Authorization: `Bearer ${token}` }
-      })
+    let username = localStorage.getItem('user');
+    axios.get(`https://myFlixDB2.herokuapp.com/users/${username}`, {
+      headers: { Authorization: `Bearer ${token}` }
+    })
       .then(response => {
         this.setState({
           userData: response.data,
@@ -50,137 +47,108 @@ export class ProfileView extends React.Component {
           favoriteMovies: response.data.FavoriteMovies
         });
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log(error);
       });
   }
 
-  deleteFavorite(e, movieId) {
-    e.preventDefault();
-    axios
-      .delete(
-        `https://myFlixDB2.herokuapp.com/users/${localStorage.getItem(
-          "user"
-        )}/movies/${movieId}`,
-        {
-          headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
-        }
-      )
+  deleteFavorite(event, movieID) {
+    event.preventDefault();
+    axios.delete(`https://myFlixDB2.herokuapp.com/users/${localStorage.getItem('user')}/movies/${movieID}`, {
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+    })
       .then(res => {
         document.location.reload(true);
       })
       .then(res => {
-        alert("Movie successfully deleted from favorites");
+        alert('Movie successfully deleted from favorites');
       })
 
       .catch(e => {
-        alert("Movie could not be deleted from favorites " + e);
+        alert('Movie could not be deleted from favorites ' + e)
       });
   }
 
   deleteProfile() {
-    axios
-      .delete(
-        `https://myFlixDB2.herokuapp.com/${localStorage.getItem("user")}`,
-        {
-          headers: { Authorization: `Bearer ${localStorage.getItem("token")}` }
-        }
-      )
-      .then(res => {
-        alert("Do you really want to delete your account?");
+    axios.delete(`https://myFlixDB2.herokuapp.com/users/${localStorage.getItem('user')}`,
+      {
+        headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
       })
       .then(res => {
-        alert("Account was successfully deleted");
+        alert('Do you really want to delete your account?')
       })
       .then(res => {
-        localStorage.removeItem("token");
-        localStorage.removeItem("user");
+        alert('Account was successfully deleted')
+      })
+      .then(res => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
 
         this.setState({
           user: null
+
         });
-        window.open("/", "_self");
+        window.open('/', '_self');
       })
       .catch(e => {
-        alert("Account could not be deleted " + e);
+        alert('Account could not be deleted ' + e)
       });
   }
 
+
+
   render() {
-    const {
-      username,
-      userProfile,
-      email,
-      birthday,
-      favoriteMovies
-    } = this.state;
+    const { username, userProfile, email, birthday, favoriteMovies } = this.state;
 
     return (
       <div className="profile-view">
         <Container>
-          <Card style={{ minwidth: "20rem" }} className="border-0 pl-0">
+          <Card style={{ minwidth: '20rem' }} className="border-0 pl-0">
             <Card.Body>
               <span className="d-flex align-items-center mb-4">
-                <Link to={`/`}>
-                  <Button
-                    variant="link"
-                    className="sign-up-link btn-lg"
-                    type="submit">
-                    Back
-                  </Button>
-                </Link>
-                <h1 className="display-4">Profile</h1>
+              <Link to={`/`}>
+                <Button variant="link" className="sign-up-link btn-lg" type="submit">Back</Button>
+              </Link>
+              <h1 className="display-4">Profile</h1>
               </span>
               <Card.Text className="mb-4 lead">
-                <span className="font-weight-bold">Username: </span>
-                {username} <br />
-                <span className="font-weight-bold">Email: </span>
-                {email} <br />
-                <span className="font-weight-bold">Birthday: </span>
-                {birthday} <br />
+                <span className="font-weight-bold">Username: </span>{username} <br />
+                <span className="font-weight-bold">Email: </span>{email} <br />
+                <span className="font-weight-bold">Birthday: </span>{birthday} <br />
               </Card.Text>
               <Link to={`/update/${username}`}>
-                <Button variant="primary" className="update-button">
-                  Update my profile
-                </Button>
+                <Button variant="primary" className="update-button">Update my profile</Button>
               </Link>
 
-              <Button
-                variant="primary"
-                className="delete-button ml-2"
-                onClick={() => this.deleteProfile()}>
-                Delete my profile
-              </Button>
+              <Button variant="primary" className="delete-button ml-2" onClick={() => this.deleteProfile()}>Delete my profile</Button>
             </Card.Body>
           </Card>
           <Container>
+
             <h4 className="mt-4 mb-4">My favorite movies: </h4>
-            {favoriteMovies.length === 0 && (
-              <div>You have no favorite movies</div>
-            )}
-            {favoriteMovies.length > 0 && (
+            {favoriteMovies.length === 0 &&
+              <div>You have no favorite movies</div>}
+            {favoriteMovies.length > 0 &&
               <ul className="ml-0 pl-0">
-                {favoriteMovies.map(movieId => (
-                  <li key={movieId} className="mb-2 ">
-                    <span className="d-flex align-items-center">
-                      <Button
-                        variant="primary"
-                        size="sm"
-                        className="delete-movie mr-2"
-                        onClick={e => this.deleteFavorite(e, movieId)}>
-                        <i className="material-icons bin">delete</i>
-                      </Button>
-                      {/* <Link to={`/movies/${movieId}`}>
-                        <h5 className="movie-link link">{movie.Title}</h5>
-                      </Link> */}
-                    </span>
-                  </li>
-                ))}
+                {favoriteMovies.map(movie =>
+                  (
+                    <li key={movie._id} className="mb-2 ">
+                      <span className="d-flex align-items-center">
+                        <Button variant="primary" size="sm" className="delete-movie mr-2" onClick={e => this.deleteFavorite(event, movie._id)}>
+                          <i className="material-icons bin">delete</i>
+                        </Button>
+                        <Link to={`/movies/${movie}`}>
+                          <h5 className="movie-link link">{movie.Title}</h5>
+                        </Link>
+
+                      </span>
+                    </li>
+                  ))}
               </ul>
-            )}
+            }
           </Container>
         </Container>
-      </div>
+      </div >
     );
   }
 }
