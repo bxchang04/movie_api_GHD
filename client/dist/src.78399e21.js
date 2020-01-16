fb74741c069be5954da2763d61895cad780537fb
bxchang04 <bxchang04@gmail.com>
2020-01-16 12:14:56 -0500

    test

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -42991,7 +42991,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51862" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54569" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
