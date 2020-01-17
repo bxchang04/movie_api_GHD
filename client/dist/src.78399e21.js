f02568066895ba2ab36e166e85ab010e24bed7f2
bxchang04 <bxchang04@gmail.com>
2020-01-17 12:47:59 -0500

    Revert "update 3.6"
    This reverts commit 0e90a45f5692baf1abbc7e9b21fbc1f8f3128395.

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -45485,7 +45485,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49837" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49569" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
