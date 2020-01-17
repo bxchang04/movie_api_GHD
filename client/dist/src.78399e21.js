570f60ab15287f93163c0b7f88f9d041d9bffdd2
bxchang04 <bxchang04@gmail.com>
2020-01-17 12:49:28 -0500

    Revert back to state after call with Hugo

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -45487,7 +45487,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63288" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49837" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
